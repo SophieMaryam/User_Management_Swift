8d41ac3beccaef6ed90d1babbed1e6a9431e2f70
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2018-05-09 10:50:11 +0200

    first commit

---
+++ User_Management/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  User_Management
+//
+//  Created by Sophie van Wersch on 23/04/2018.
+//  Copyright © 2018 Sophie van Wersch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
